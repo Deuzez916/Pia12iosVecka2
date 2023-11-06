c815037b9e154c8ea0145d40abf3f5cda8ba2715
Filip Nicklasson <filip@nicklasson.nu>
2023-11-06 09:29:04 +0100

    More Text

--- Pia12iosVecka2/ContentView.swift
+++ Pia12iosVecka2/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, World!")
+            Text("More Text")
         }
         .padding()
     }
