4a237fac54aded77e332402e98e987d6f5ec6da1
Filip Nicklasson <filip@nicklasson.nu>
2023-11-06 09:08:41 +0100

    Initial Commit

---
+++ Pia12iosVecka2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pia12iosVecka2
+//
+//  Created by Filip Nicklasson on 2023-11-06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pia12iosVecka2/Pia12iosVecka2App.swift
@@ -0,0 +1,17 @@
+//
+//  Pia12iosVecka2App.swift
+//  Pia12iosVecka2
+//
+//  Created by Filip Nicklasson on 2023-11-06.
+//
+
+import SwiftUI
+
+@main
+struct Pia12iosVecka2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
